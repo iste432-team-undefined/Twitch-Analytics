6ec2d81775d9627ff95e6bc7f8543d83d60a03ea
Kenny S <35939549+kxs6629@users.noreply.github.com>
2020-11-05 20:51:47 -0500

    Updated postdb.js
    Updated postdb.js so that it implements promises. Building functions to test

--- postdb.js
+++ postdb.js
@@ -8,36 +8,69 @@ const pool = new Pool({
     password: 'password',
     port: 5432,
   })
-// **** Maybe deprecated?? ****
-// function connectPool(){
-//     pool.connect((err,client,done) =>{
-//         if(err){
-//             console.log(err);
-//         }
-//     });
-// }
-// function getUser(id){
-//     pool.query('SELECT username FROM "user" WHERE id_user=($1)',(err,res) =>{
-//         if (err){
-//             console.log(err)
-//         }
-//         else{
-//             console.log(res);
-//         }
-//     });
-// }
-// connectPool();
-// getUser(1);
 
+function testConn() {
+  const query = {
+      // give the query a unique name
+      name: 'get-user',
+      text: 'SELECT username FROM "user" WHERE id_user = $1',
+      values: [1],
+      }
 
-const query = {
-    // give the query a unique name
-    name: 'get-user',
-    text: 'SELECT username FROM "user" WHERE id_user = $1',
-    values: [1],
+  
+  return new Promise( (resolve, reject) => {
+      pool.query(query, (err, result) => {
+          if (err) {
+              return reject(err);
+          }
+          resolve(result);
+      });
+  });
+}
+
+function getUser(id){
+  const nameQuery = {
+    name: 'get-name',
+    text: format('SELECT username from "user" WHERE id_user = $1'),
+    values:[id],
   }
 
-pool
-  .query(query)
-  .then(res => console.log(res.rows[0]))
-  .catch(e => console.error(e.stack))
+  return new Promise( (resolve, reject) => {
+    pool.query(nameQuery, (err, result) => {
+        if (err) {
+            return reject(err);
+        }
+        resolve(result);
+    });
+  });
+}
+
+function getDashboardName(id){
+  const dashQuery = {
+    name: 'get-dash',
+    text: format('SELECT title FROM dashboard WHERE id_dashboard=$1'),
+    values:[id],
+  }
+
+  return new Promise( (resolve, reject) => {
+    pool.query(dashQuery, (err,result) =>{
+      if(err){
+        return reject(err);
+      }
+      resolve(result);
+    });
+  });
+}
+
+testConn().then( (res) => {
+  console.log(res.rows[0]) ;
+}).catch( (err) => setImmediate(() => { throw err; }));
+
+
+getUser(1).then( (res) => {
+  console.log(res.rows[0]);
+}).catch( (err) => setImmediate(() => {throw err; }));
+
+getDashboardName(1).then( (res) =>{
+  console.log(res.rows[0]);
+}).catch( (err) => setImmediate(() => {throw err;}));
