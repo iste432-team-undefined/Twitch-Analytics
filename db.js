af7618b8b9c940ff6b911d3e8c29439d3afd911f
Kenny S <35939549+kxs6629@users.noreply.github.com>
2020-11-05 21:24:16 -0500

    Removed mysql
    goodbye mysql

--- db.js
+++ db.js
@@ -13,7 +13,7 @@ function testConn() {
   const query = {
       // give the query a unique name
       name: 'get-user',
-      text: 'SELECT username FROM "user" WHERE id_user = $1',
+      text: format('SELECT username FROM "user" WHERE id_user = $1'),
       values: [1],
       }
 
@@ -23,7 +23,7 @@ function testConn() {
           if (err) {
               return reject(err);
           }
-          resolve(result);
+          resolve(result.rows);
       });
   });
 }
@@ -40,7 +40,7 @@ function getUser(id){
         if (err) {
             return reject(err);
         }
-        resolve(result);
+        resolve(result.rows);
     });
   });
 }
@@ -57,19 +57,49 @@ function getDashboardName(id){
       if(err){
         return reject(err);
       }
+      resolve(result.rows);
+    });
+  });
+}
+
+function createDashboard(name,ownerID){
+  const createDashQuery = {
+    name: 'make-dash',
+    text: format('INSERT INTO dashboard (title, owner_id) VALUES ($1,$2)'),
+    values: [name,ownerID],
+  }
+  return new Promise( (resolve, reject) =>{
+    pool.query(createDashQuery, (err,result) => {
+      if (err) {
+        return reject(err);
+      }
       resolve(result);
     });
   });
 }
 
 testConn().then( (res) => {
-  console.log(res.rows[0]) ;
+  console.log(res[0]) ;
 }).catch( (err) => setImmediate(() => { throw err; }));
 
 getUser(1).then( (res) => {
-  console.log(res.rows[0]);
+  console.log(res[0]);
 }).catch( (err) => setImmediate(() => {throw err; }));
 
-getDashboardName(1).then( (res) =>{
-  console.log(res.rows[0]);
+getDashboardName(1).then( (res) => {
+  console.log(res[0]);
 }).catch( (err) => setImmediate(() => {throw err;}));
+
+createDashboard("test",2).then( (res) => {
+}).catch( (err) => setImmediate(() => {throw err; }));
+
+getDashboardName(3).then( (res) => {
+  console.log(res[0]);
+}).catch( (err) => setImmediate(() => {throw err;}));
+
+module.exports = {
+  testConn,
+  getUser,
+  getDashboardName,
+  createDashboard
+}
