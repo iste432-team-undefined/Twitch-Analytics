950660f247f7beaeb3f27be46c8905c918788921
Zach <zacheas@outlook.com>
2020-11-13 13:18:59 -0500

    Fixed twitch api client query

--- index.js
+++ index.js
@@ -3,13 +3,12 @@ const express = require('express') ;
 const app = new express();
 
 const client_id = '7j49l2zp0qud3b2ppqboximy6uqjl9';
-const client_secret = 'kxj7yegz4m53ljythes3xkpnhy3r2t' ;
+const client_secret = '3cel4zw4sy6kjhozvu9dvuc7njml4v' ;
 const grant_type = 'client_credentials' ;
 
 const twitch_api = 'https://api.twitch.tv/helix/' ;
 
-const test_id = 89490179 ;
-
+// MUST BE CALLED TO GET A GOOD OAUTH TOKEN
 getTwitchRITClientAccessToken(tokenRecieved) ;
 
 function getTwitchRITClientAccessToken (callback) {
@@ -18,27 +17,30 @@ function getTwitchRITClientAccessToken (callback) {
     .then(res=> {
         callback(res.data.access_token) ;
     });
-} 
-
-function tokenRecieved(client_data) {
-    console.log(client_data) ;
-    testTwitchApi(client_data) ;
-    getUserToken(client_data.access_token, display);
 }
 
-function testTwitchApi(data, callback) {
+// This is where we do things AFTER we get a client token
+function tokenRecieved(client_token) {
+    console.log("Client Token: " + client_token) ;
+
+    // This call is where we get twitch user information from a username
+    getTwitchUser(client_token, "shroud", display) ;
+}
+
+// This actually makes the call to the twitch api and callsback to a display function to push to the frontend
+function getTwitchUser(token, username, callback) {
     let config = {
         headers: {
-            Authorization: 'Bearer '+data.access_token,
+            Authorization: 'Bearer '+token,
             'Client-Id': client_id
         }
     }
 
     axios
-        .get(twitch_api+'users/follows?from_id=89490179',config)
+        .get(twitch_api+'users?login='+ username, config)
         .then(res => {
-           // console.log(res);
             console.log(res.data);
+            callback(res.data);
         })
         .catch(err => {
             console.log(err) ;
@@ -46,24 +48,8 @@ function testTwitchApi(data, callback) {
 }
 
 
-function getUserToken(client_token, callback) {
-    let config = {
-        headers: {
-            'Access-Control-Allow-Origin': 'http://localhost'
-        }
-    }
-
-
-    axios
-        .get('https://id.twitch.tv/oauth2/authorize?response_type=token&client_id='+client_id+'&redirect_uri=http://localhost&scope=viewing_activity_read&state='+client_token, config)
-        .then(res => {
-            callback(res);
-        });
-}
-
 function display(data) {
     app.get('/', function(req, res) {
-        console.log(res) ;
         res.send(data.data) ;
     });
 }
