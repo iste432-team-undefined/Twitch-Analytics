2c778666acba1d9f1977bafa17d9271030c960bd
kxs6629 <35939549+kxs6629@users.noreply.github.com>
2020-11-13 13:06:44 -0500

    Modified database script to contain more testing data. Will work on implementing data from the API itself now that our queries to it are working.

--- twitch_analytic_postgres.sql
+++ twitch_analytic_postgres.sql
@@ -13,7 +13,7 @@ CREATE TABLE "user" (
 DROP TABLE IF EXISTS dashboard;
 CREATE TABLE dashboard (
 	id_dashboard SERIAL,
-	title VARCHAR(20),
+	title VARCHAR(45),
 	owner_id INT NOT NULL,
 	PRIMARY KEY(id_dashboard),
 	FOREIGN  KEY (owner_id) REFERENCES "user" (id_user)
@@ -45,18 +45,42 @@ CREATE TABLE user_dashboard (
 	FOREIGN KEY (id_dashboard) REFERENCES dashboard (id_dashboard)
 );
 
-INSERT INTO "user" (username,password,salt) VALUES ('test1','testpwd','salthere');
-INSERT INTO "user" (username,password,salt) VALUES ('test2','estpw','alhere');
-INSERT INTO "user" (username,password,salt) VALUES ('test3','ttpd','sthere');
+INSERT INTO "user" (username,password,salt) VALUES ('gamer_man1','testpwd','salthere');
+INSERT INTO "user" (username,password,salt) VALUES ('Xxx_gamer420_xxX','estpw','alhere');
+INSERT INTO "user" (username,password,salt) VALUES ('GgeZ','ttpd','sthere');
 
 INSERT INTO dashboard (title, owner_id) VALUES ('StreamerName', 2);
 INSERT INTO dashboard (title, owner_id) VALUES ('GameName', 1);
+INSERT INTO dashboard (title, owner_id) VALUES ('Minecraft ', 1);
+INSERT INTO dashboard (title, owner_id) VALUES ('League of Legends', 1);
+INSERT INTO dashboard (title, owner_id) VALUES ('Skate 4', 3);
+INSERT INTO dashboard (title, owner_id) VALUES ('Ninja', 2);
+INSERT INTO dashboard (title, owner_id) VALUES ('Escape from Tarkov', 3);
+INSERT INTO dashboard (title, owner_id) VALUES ('Call of Duty Warfare Warfare', 3);
 
 INSERT INTO "view" (view_type,id_dashboard,content_id) VALUES (1,1,3125543);
-INSERT INTO "view" (view_type,id_dashboard,content_id) VALUES (2,1,4512354);
+INSERT INTO "view" (view_type,id_dashboard,content_id) VALUES (2,2,4512354);
+INSERT INTO "view" (view_type,id_dashboard,content_id) VALUES (2,3,4512354);
+INSERT INTO "view" (view_type,id_dashboard,content_id) VALUES (2,4,7512352);
+INSERT INTO "view" (view_type,id_dashboard,content_id) VALUES (2,5,9582657);
+INSERT INTO "view" (view_type,id_dashboard,content_id) VALUES (1,6,1512337);
+INSERT INTO "view" (view_type,id_dashboard,content_id) VALUES (2,7,8352554);
+INSERT INTO "view" (view_type,id_dashboard,content_id) VALUES (2,8,3522904);
 
 INSERT INTO dashboard_view (id_dashboard,id_view) VALUES (1, 1);
-INSERT INTO dashboard_view (id_dashboard,id_view) VALUES (2, 1);
+INSERT INTO dashboard_view (id_dashboard,id_view) VALUES (2, 2);
+INSERT INTO dashboard_view (id_dashboard,id_view) VALUES (3, 3);
+INSERT INTO dashboard_view (id_dashboard,id_view) VALUES (4, 4);
+INSERT INTO dashboard_view (id_dashboard,id_view) VALUES (5, 5);
+INSERT INTO dashboard_view (id_dashboard,id_view) VALUES (6, 6);
+INSERT INTO dashboard_view (id_dashboard,id_view) VALUES (7, 7);
+INSERT INTO dashboard_view (id_dashboard,id_view) VALUES (8, 8);
 
-INSERT INTO user_dashboard (id_user, id_dashboard) VALUES (1,1);
-INSERT INTO user_dashboard (id_user, id_dashboard) VALUES (2,2);
+INSERT INTO user_dashboard (id_user, id_dashboard) VALUES (2,1);
+INSERT INTO user_dashboard (id_user, id_dashboard) VALUES (1,2);
+INSERT INTO user_dashboard (id_user, id_dashboard) VALUES (1,3);
+INSERT INTO user_dashboard (id_user, id_dashboard) VALUES (1,4);
+INSERT INTO user_dashboard (id_user, id_dashboard) VALUES (3,5);
+INSERT INTO user_dashboard (id_user, id_dashboard) VALUES (2,6);
+INSERT INTO user_dashboard (id_user, id_dashboard) VALUES (3,7);
+INSERT INTO user_dashboard (id_user, id_dashboard) VALUES (3,8);
